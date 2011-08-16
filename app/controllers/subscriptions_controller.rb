482cc63533e9a7e2da7193e73ff341bb885743f3
Kathryn Aaker <kathryn@kathrynaaker.com>
2011-08-15 19:54:13 -0700

    Sorting subscriptions by last name

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,6 +2,7 @@ class SubscriptionsController < ApplicationController
   def index
     @product = Product.find(params[:product_id])
     @subscriptions = Subscription.find_all_by_product_id(@product.id)
+    @subscriptions.sort! {|x,y| x.account.member.last_name <=> y.account.member.last_name }
   end
 
   def show
