0613cfca30619eccb1d6cc6da6dece175e9847af
Kathryn Aaker <kathryn@kathrynaaker.com>
2011-08-15 18:57:35 -0700

    Added SubscriptionTransaction importer

---
+++ app/models/subscription_transaction_importer.rb
@@ -0,0 +1,60 @@
+class SubscriptionTransactionImporter
+
+  def generate_transactions!(deliveries, perform=false)
+    count = 0
+    puts "perform: #{perform}"
+    deliveries.each do |delivery|
+      delivery.orders.each do |order|
+        order.order_items.each do |item|
+          if item.stock_item.product
+            if item.stock_item.product.subscribable == true && item.quantity > 0
+              if order.member.account_for_farm(delivery.farm).subscriptions.size == 1
+                subscription = order.member.account_for_farm(delivery.farm).subscriptions.first
+
+                trans = SubscriptionTransaction.new
+                trans.date = delivery.date
+                trans.description = "CSA Pickup: #{delivery.name} #{delivery.pretty_date}"
+                trans.amount = item.quantity
+                trans.debit = true
+                trans.subscription_id = subscription.id
+                count = count + 1
+                if perform
+                  trans.save!
+                else
+                  puts "---"
+                  puts item.stock_item.product_name
+                  puts order.member.last_name
+                  puts trans.date
+                  puts trans.amount
+                  puts trans.description
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+    puts "Total Count: #{count}"
+    return count
+  end
+
+  def output_mismatches(deliveries)
+
+    deliveries.each do |delivery|
+      delivery.orders.each do |order|
+        order.order_items.each do |item|
+          if item.stock_item.product
+            if item.stock_item.product.subscribable && item.quantity > 0
+              if order.member.account_for_farm(delivery.farm).subscriptions.size != 1
+                puts "member does not have subscription:"
+                puts order.member.last_name
+                puts order.id
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+end
