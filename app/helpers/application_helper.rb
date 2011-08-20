9dbc877b1096140d3a4bd72160d91e7c7bc1a6f9
Kathryn Aaker <kathryn@kathrynaaker.com>
2011-08-19 23:22:21 -0700

    Date formatting helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,15 @@ module ApplicationHelper
     messages = objects.compact.map {|o| o.errors.full_messages}.flatten
     render :partial => 'error_messages', :object => messages unless messages.empty?
   end
+
+  def format_date(date, use_time = false)
+    if use_time
+      ampm = date.strftime("%p").downcase
+      date.strftime("%B %d, %Y at %I:%M" + ampm)
+    else
+      date.strftime("%B %d, %Y")
+    end
+  end
+
+
 end
