b93319e6487119080841a10c5250b4745aa5f04c
Kathryn Aaker <kathryn@kathrynaaker.com>
2011-08-24 13:20:24 -0700

    Whitespace fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -13,7 +13,7 @@ class ApplicationMailer < ActionMailer::Base
     params[:bcc] = options['bcc'] if options.key?('bcc')
     
     mail params do |format|
-  format.text { render :text => email_template.render_body(options) }
+      format.text { render :text => email_template.render_body(options) }
     end
 
   end
