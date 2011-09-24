b94cb3f94d87973f32d6769015f47c779411db8b
Kathryn Aaker <kathryn@kathrynaaker.com>
2011-09-24 09:30:33 -0700

    Fixed check for product before setting subscribable

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -5,7 +5,7 @@ module OrdersHelper
     return quantity_visibility if current_user.has_role?(:admin)
 
     product = order_item.stock_item.product
-    is_subscribable = product.subscribable
+    is_subscribable = product.nil? ? false : product.subscribable
     account = member.account_for_farm(@farm)
 
     subscription_visibility = is_subscribable ? account.has_subscription?(product) : true
